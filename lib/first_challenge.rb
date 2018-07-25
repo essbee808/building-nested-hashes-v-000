a45292bc45b3f4785315f59d521957b85a627ab0
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-25 05:12:46 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
       :patriarch => {:name => "Lord Capulet", :age => "50"},
       :matriarch => {:name => "Lady Capulet", :age => "51"},
       :heroine => {:name => "Juliet", :age => "15", :status => "Alive"},
-      :hero_friends => {}
+      :heroine_friends => {}
      }
    }
 end
