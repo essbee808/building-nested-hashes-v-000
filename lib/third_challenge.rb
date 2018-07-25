84ec3caa40d5352c697b266711546ea0bf24df0c
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-25 05:23:21 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -5,7 +5,7 @@ def third_challenge
         :name => "Lord Montague", :age => "53"
         },
       :matriarch => {
-        :name => "Lord Montague", :age => "53"
+        :name => "Lady Montague", :age => ""
         },
       :hero => {
         #your key/value pairs here
