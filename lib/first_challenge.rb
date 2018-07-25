9abeaf36e9b443e15b41044b0c0fffbe03e512ac
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-25 05:18:59 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -8,24 +8,3 @@ def first_challenge
   
 end
   
-  # epic_tragedy = {
-  #   :montague => {
-  #     :patriarch => {:name => "Lord Montague", :age => "53"},
-  #     :matriarch => {:name => "Lady Montague", :age => "54"},
-  #     :hero => {:name => "Romeo", :age => "15", :status => "Alive"},
-  #     :hero_friends => {
-  #       :friend_1 => {:name => "Benvolio", :age => "17", :attitude => "worried"}, 
-  #       :friend_2 => {:name => "Mercutio", :age => "18", :attitude => "hot-headed"}
-  #     }
-  #   },
-     
-  #   :capulet => {
-  #     :patriarch => {:name => "Lord Capulet", :age => "50"},
-  #     :matriarch => {:name => "Lady Capulet", :age => "51"},
-  #     :heroine => {:name => "Juliet", :age => "15", :status => "Alive"},
-  #     :heroine_friends => {
-  #       :friend_1 => {:name => "Steven", :age => "30", :attitude => "confused"},
-  #       :friend_2 => {:name => "Nurse", :age => "44", :attitude => "worried"}
-  #     }
-  #   }
-  # }
