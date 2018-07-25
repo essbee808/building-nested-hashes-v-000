021cad985a0f9c4dfef35efd4666c1ec33b95cf3
Sheena Bonilla <sheena.myb@gmail.com>
2018-07-25 04:57:23 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,9 @@
 def first_challenge
    epic_tragedy = {
-     :montague => {:patriarch => {:name => "Lord Montague", :age => "53"}},
+     :montague => {
+       :patriarch => {:name => "Lord Montague", :age => "53"}
+       :matriarch => {:name => "Lady Montague", :age => "54"}
+     },
      
      :capulet => {}
    }
